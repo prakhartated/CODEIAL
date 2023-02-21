eb7a055461fd328b3e80f10b84a9cf06f07360ce
Prakhar Tated <64362445+prakhartated@users.noreply.github.com>
2023-02-21 20:37:08 +0530

    home & users views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.profile = function (req, res) {
-  res.end("<h1>Users Profile</h1>");
+  return res.render("users" , {
+    title: "Users"
+  });
 };
 
 module.exports.posts = function (req , res) {
