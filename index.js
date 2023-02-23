ec3e8c2d062778ace5d8bf3dd011987cd1d0fd7a
Prakhar Tated <64362445+prakhartated@users.noreply.github.com>
2023-02-24 01:51:09 +0530

    implementing layout

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
 // use express router
+app.use(expressLayouts);
 app.use('/' , require('./routes/index'));
+
 
 app.set('view engine' , 'ejs');
 app.set('views' , './views');
