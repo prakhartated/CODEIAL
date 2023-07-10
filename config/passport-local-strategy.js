b0dc951be3638a9069dc944f9620fe8145981640
Prakhar Tated <64362445+prakhartated@users.noreply.github.com>
2023-07-10 08:43:16 +0530

    Setting Current Authentication

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -40,4 +40,22 @@ passport.deserializeUser(function (id , done) {
     });
 });
 
+// check if the user is authenticated
+passport.checkAuthentication = function (req , res , next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    // if the user is not signed in
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser = function (req , res , next) {
+    if (req.isAuthenticated()) {
+        res.locals.user = req.user;
+    }
+
+    next();
+}
+
 module.exports = passport;
--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportlocal = require('./config/passport-local-strategy');
 
 app.use(express.urlencoded());
 
@@ -16,14 +20,27 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-
-// use express router
-app.use('/', require('./routes'));
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if (err){
